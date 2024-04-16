e75936f5fe44769a3839fac7b0984280fc8f89b0
damngz <da.munozgz@gmail.com>
2024-04-15 21:34:38 -0400

    chore: base project

---
+++ src/main/java/com/s2/atentions/AtentionsApplication.java
@@ -0,0 +1,13 @@
+package com.s2.atentions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtentionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtentionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s2/atentions/AtentionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2.atentions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtentionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
